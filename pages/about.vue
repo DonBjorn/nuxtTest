83d7b665df22aa82d0026cc57075dd5061adc9b6
franciscot <francisco.tapia.t@gmail.com>
2020-11-04 19:43:26 -0300

    Primer Commit

---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>About - Hola Mundo</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+
+    <div class="col-6">
+      <b-input placeholder="Hola"></b-input>
+      <NuxtLink to="/about">About</NuxtLink>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
